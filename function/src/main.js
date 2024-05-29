32144feff446517751c2bfb1710cb18da12b7af7
Nichole McGrew <nicholemcgrew@gmail.com>
2024-05-29 11:24:15 -0500

    _

---
+++ function/src/main.js
@@ -0,0 +1,25 @@
+import AppExpress from "@itznotabug/appexpress"
+const app = new AppExpress()
+
+const getRoutes = (request, response) => {
+    response.json({'routes': ['/', '/user/:username', '/user']})
+}
+
+const getUser = (request, response) => {
+    const {username} = request.params
+    response.json({'username':username})
+}
+
+const createUser = (request, response) => {
+    response.json({'postData':request.body})
+}
+
+app.get('/', getRoutes)
+app.get("/user/:username", getUser)
+app.post("/user", createUser)
+
+export default async (context) => await app.attach(context)
+
+// export default async ({ req, res, log, error }) => {
+//     return res.send("hello world")
+// }
